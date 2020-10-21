aeb1fd9d70b4872db791628bfc4d8f9e252a2fe4
luzybry94 <luzybry94@gmail.com>
2020-10-20 17:09:27 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Chaim Bryski"
+    end
+
+    get '/hometown' do 
+        "My hometown is Agoura Hills, CA"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Before You Go"
+    end
+
+
 end
